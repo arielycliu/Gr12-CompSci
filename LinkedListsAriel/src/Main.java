88e2dab086cc5c78abde13fc529b9a41b7a46cbf
arielycliu <ariel.yc.liu@gmail.com>
2021-09-23 10:02:06 -0400

    Added RemovefromRear and RemovefromFront

--- LinkedListsAriel/src/Main.java
+++ LinkedListsAriel/src/Main.java
@@ -7,16 +7,25 @@ public class Main {
 		
 		theStudent = new StudentInfo(123456, "Ari", "Liu");		
 		theLL.addToFront(theStudent);
-		theLL.displayList();
 		
 		theStudent = new StudentInfo(656789, "Jan", "An");
 		theLL.addToRear(theStudent);
-		theLL.displayList();
 		
 		theStudent = new StudentInfo(656789, "Pat", "Li");
 		theLL.addToFront(theStudent);
+		
 		theLL.displayList();
 		
+		StudentInfo removedItem;
+		removedItem = theLL.removeFromRear();
+		
+		if (removedItem != null) {
+			System.out.println("Removed: " + removedItem.firstName);
+		} else {
+			System.out.println("CANNOT BE REMOVED, LIST IS EMPTY");
+		}
+		
+		theLL.displayList();
 		
 	}
 }
