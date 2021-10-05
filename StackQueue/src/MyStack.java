156e7068213e2b23fa1136e5f5833349e9057a3b
arielycliu <ariel.yc.liu@gmail.com>
2021-10-05 14:22:12 -0400

    stackqueue files

---
+++ StackQueue/src/MyStack.java
@@ -0,0 +1,17 @@
+//MyStack
+//
+//Has attributes top and numInStack.  
+//The stack is initialized (via the constructor) to being empty.
+//
+//Has methods push, pop, peek, displayStack.  
+//push and displayStack are to be void methods.  
+//pop and peek return reference to the item popped/peeked, 
+//and return null if the stack is empty.  
+//displayStack is to use System.out.println to walk through 
+//and print the students in the stack from top to bottom 
+//(and is to say that the stack is empty if it is indeed empty).
+
+
+public class MyStack {
+
+}
---
+++ StackQueue/src/TestStackQueue.java
@@ -0,0 +1,67 @@
+
+public class TestStackQueue {
+
+	public static void main(String[] args) {
+
+		MyStack theS = new MyStack();
+		MyQueue theQ = new MyQueue();
+		
+		
+		StudentInfo someStudent;
+		StudentInfo removedStudent;
+		
+		
+// STACK
+		System.out.println("\n\nTESTING THE STACK STUFF");
+		
+		System.out.println("\nAbout to display the stack (top to bottom)!");
+		theS.displayStack();
+				
+		someStudent = new StudentInfo(111111, "Daffy", "Duck", 20, 10);
+		theS.push(someStudent);
+
+		System.out.println("\nAbout to display the stack (top to bottom)!");
+		theS.displayStack();
+		
+		removedStudent = theS.pop();
+		if (removedStudent == null) {
+			System.out.println("\npopped item is null");
+		}
+		else {
+			System.out.println("\npopped item is " + removedStudent.firstName);
+			System.out.println("attribute next has value " + removedStudent.next);
+		}
+		System.out.println("\nAbout to display the stack (top to bottom)!");
+		theS.displayStack();
+		
+		
+// QUEUE
+		System.out.println("\n\nTESTING THE QUEUE STUFF");
+		
+		
+		System.out.println("\nAbout to display the queue (front to rear)!");
+		theQ.displayQueue();
+		
+		
+		someStudent = new StudentInfo(111111, "Daffy", "Duck", 20, 10);
+		theQ.enqueue(someStudent);
+		
+		System.out.println("\nAbout to display the queue (front to rear)!");
+		theQ.displayQueue();
+		
+		
+		removedStudent = theQ.dequeue();
+		if (removedStudent == null) {
+			System.out.println("\ndequeued item is null");
+		}
+		else {
+			System.out.println("\ndequeued item is " + removedStudent.firstName);
+			System.out.println("attribute next has value " + removedStudent.next);
+		}
+		System.out.println("\nAbout to display the queue (front to rear)!");
+		theQ.displayQueue();
+
+
+	}  // end main
+
+}
