2bd09f1314771be6d1985009de1f7646a565cfaf
arielycliu <ariel.yc.liu@gmail.com>
2021-10-05 14:22:59 -0400

    stackqueue files: stack - display and push

--- StackQueue/src/MyStack.java
+++ StackQueue/src/MyStack.java
@@ -13,5 +13,48 @@
 
 
 public class MyStack {
-
+	
+	//attributes
+	public int numInStack;
+	public StudentInfo top;
+	
+	// init -> constructor
+	public MyStack() {
+		numInStack = 0;
+		top = null;
+	}
+	
+	// displayStack - print out each student
+	public void displayStack() {
+		if (top == null) {  // if the list is empty
+			System.out.println("\nEMPTY LIST");
+			return;
+		}
+		// else: the list must have stuff inside
+		System.out.println("\nWe have " + numInStack + " elements: ");
+		StudentInfo currentStudent = top;
+		
+		// loop thru to print names
+		for (int i = 0; i < numInStack; i++) {
+			System.out.println(currentStudent.firstName);
+			currentStudent = currentStudent.next;
+		}
+		return;
+	}
+	// push - put an item onto the top of the stack
+	public void push(StudentInfo studentToAdd) {
+		if (top == null) {  // if the list is empty
+			studentToAdd.next = null; // make sure the student isn't pointing to anything since it's the only thing in the list
+			top = studentToAdd;
+			numInStack = 1;
+			return;			
+		}
+		// else: the list must have stuff in it already
+		studentToAdd.next = top; // have the new top point to the previous top (now second)
+		top = studentToAdd;
+		numInStack++; // make sure to increase count by 1
+		return;
+	}
+	// pop - remove item from the top of the stack - return ref value
+	
 }
