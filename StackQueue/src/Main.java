ddeb789bb344c2d8c06df812892f22329f13ead1
arielycliu <ariel.yc.liu@gmail.com>
2021-10-05 14:35:16 -0400

    Stack Completed

--- StackQueue/src/Main.java
+++ StackQueue/src/Main.java
@@ -6,7 +6,36 @@ public class Main {
 		StudentInfo theStudent;
 		theStudent = new StudentInfo(345345, "Ari", "Liu", 162, 115);
 		
-		System.out.println(theStudent.weight);
+		MyStack theS = new MyStack();
+		MyQueue theQ = new MyQueue();
+		
+		
+		StudentInfo someStudent;
+		StudentInfo removedStudent;
+		
+		
+// STACK
+		System.out.println("\n\nTESTING THE STACK STUFF");
+		
+		System.out.println("\nAbout to display the stack (top to bottom)!");
+		theS.displayStack();
+				
+		someStudent = new StudentInfo(111111, "Daffy", "Duck", 20, 10);
+		theS.push(someStudent);
+
+		System.out.println("\nAbout to display the stack (top to bottom)!");
+		theS.displayStack();
+		
+		removedStudent = theS.pop();
+		if (removedStudent == null) {
+			System.out.println("\npopped item is null");
+		}
+		else {
+			System.out.println("\npopped item is " + removedStudent.firstName);
+			System.out.println("attribute next has value " + removedStudent.next);
+		}
+		System.out.println("\nAbout to display the stack (top to bottom)!");
+		theS.displayStack();
 		
 	}
 }
