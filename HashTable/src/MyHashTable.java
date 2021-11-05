c1b0c1d73ba267d02a2b13c48169204dbff982a6
Ariel <ariel.yc.liu@gmail.com>
2021-11-05 00:18:42 -0400

    comments and check

--- HashTable/src/MyHashTable.java
+++ HashTable/src/MyHashTable.java
@@ -35,10 +35,9 @@ public class MyHashTable {
 
 	public void addToTable(StudentInfo theStudent) {
 		// Add the student referenced by theStudent to the hash table.
-		int bucketNum = calcBucket(theStudent.studentNumber); // find which bucket to place it in
+		int bucketNum = calcBucket(theStudent.studentNumber); // find which bucket to place it in using it's student number
 		buckets[bucketNum].add(theStudent);
 		return;
-		
 	}
 
 
@@ -46,40 +45,39 @@ public class MyHashTable {
 		// Remove that student from the hash table and return the reference value for that student.
 		// Return null if that student isn't in the table.
 		
-		int bucketNum = calcBucket(studentNumber);
+		int bucketNum = calcBucket(studentNumber);    // find which bucket we saved that student to
 		
-		for (int i = 0; i < buckets[bucketNum].size(); i++) { // loop thru that bucket index, that arraylist
-			if (buckets[bucketNum].get(i).studentNumber == studentNumber) { // check if it matches the student number
-				StudentInfo removedStudent = buckets[bucketNum].get(i); // save the studentinfo ref val so we don't lose it
-				buckets[bucketNum].remove(i);
-				return removedStudent; // return if it does
+		for (int i = 0; i < buckets[bucketNum].size(); i++) {    // loop thru that bucket's ArrayList
+			if (buckets[bucketNum].get(i).studentNumber == studentNumber) {     // check if the element in the ArrayList has the same student number
+				StudentInfo removedStudent = buckets[bucketNum].get(i);      // save the student's reference value so we can return it
+				buckets[bucketNum].remove(i);      // remove the student
+				return removedStudent; // return the student with the matching studentNumber
 			}
 		}
-		return null; // could not find student, return null
+		return null; // could not find student after checking the bucket, return null
 	}
 
 	
 	public StudentInfo getFromTable(int studentNumber) {
 		// Return the reference value for that student, return null if student isn't in the table.
-		int bucketNum = calcBucket(studentNumber);
+		int bucketNum = calcBucket(studentNumber);    // find which bucket we saved that student to
 		
-		// check that bucketNum or bucket index for student with that studentNumber
-		for (int i = 0; i < buckets[bucketNum].size(); i++) { // loop thru that bucket index, that arraylist
-			// System.out.println(buckets[bucketNum].get(i));
-			if (buckets[bucketNum].get(i).studentNumber == studentNumber) { // check if it matches the student number
-				return buckets[bucketNum].get(i); // return if it does
+		// check that bucketNum or bucket index for the student with that studentNumber
+		for (int i = 0; i < buckets[bucketNum].size(); i++) {     // loop thru that bucket's ArrayList
+			if (buckets[bucketNum].get(i).studentNumber == studentNumber) { // check if that student in the ArrayList matches the student number
+				return buckets[bucketNum].get(i);    // return this student if it does match
 			}
 		}
-		return null; // could not find student, return null
+		return null; // could not find student after checking the bucket, return null
 	}
 
 
 	public void displayTable() {
 		// Walk through the buckets and display the items in each bucket's ArrayList.
 		System.out.println("##############################");
-		for (int j = 0; j < buckets.length; j++) { // loop thru each bucket/arrayList
+		for (int j = 0; j < buckets.length; j++) {    // loop thru each bucket of the hash table
 			System.out.println("~~~ Bucket " + j + " ~~~");
-			for (int i = 0; i < buckets[j].size(); i++) { // loop thru each arrayList
+			for (int i = 0; i < buckets[j].size(); i++) {     // loop thru each element in the ArrayList of each bucket
 				System.out.println(buckets[j].get(i).firstName);
 			}
 		}
--- HashTable/src/TestHashTable.java
+++ HashTable/src/TestHashTable.java
@@ -9,29 +9,42 @@ public class TestHashTable {
 		
 		MyHashTable theHT = new MyHashTable(10);  // Open hash table with 10 buckets
 		
-		theHT.displayTable();
+//		theHT.displayTable();
 		
-		someStudent = new StudentInfo(123456, "Bugs", "Bunny", 40, 40);
+		someStudent = new StudentInfo(111111, "Bugs", "Bunny", 40, 40);
 		theHT.addToTable(someStudent);
 		
-		someStudent = new StudentInfo(987654, "Lola", "Bunny", 40, 40);
+		someStudent = new StudentInfo(111112, "Lola", "Bunny", 40, 40);
 		theHT.addToTable(someStudent);
 		
-		someStudent = new StudentInfo(464646, "Daffy", "Duck", 40, 40);
+		someStudent = new StudentInfo(111113, "Daffy", "Duck", 40, 40);
 		theHT.addToTable(someStudent);
 		
-		someStudent = new StudentInfo(678904, "Pat", "Duck", 40, 40);
+		someStudent = new StudentInfo(111114, "Pat", "Duck", 40, 40);
 		theHT.addToTable(someStudent);
 		
-		someStudent = new StudentInfo(521432, "Jan", "Duck", 40, 40);
+		someStudent = new StudentInfo(111115, "Jan", "Duck", 40, 40);
 		theHT.addToTable(someStudent);
 		
-		someStudent = new StudentInfo(563452, "Ari", "Duck", 40, 40);
+		someStudent = new StudentInfo(111116, "Ari", "Duck", 40, 40);
+		theHT.addToTable(someStudent);
+		
+		someStudent = new StudentInfo(111117, "Fan", "Duck", 40, 40);
+		theHT.addToTable(someStudent);
+		
+		someStudent = new StudentInfo(111118, "Dan", "Duck", 40, 40);
+		theHT.addToTable(someStudent);
+		
+		someStudent = new StudentInfo(111119, "Tan", "Duck", 40, 40);
+		theHT.addToTable(someStudent);
+		
+		someStudent = new StudentInfo(111120, "Han", "Duck", 40, 40);
+		theHT.addToTable(someStudent);
+		
+		someStudent = new StudentInfo(111120, "Han2", "Duck", 40, 40);
 		theHT.addToTable(someStudent);
 
-		theHT.displayTable();
-		someStudent = theHT.getFromTable(678904);
-		
+		someStudent = theHT.getFromTable(111111);
 		if (someStudent != null) {
 			System.out.println(someStudent.firstName);
 		}
@@ -39,8 +52,8 @@ public class TestHashTable {
 			System.out.println(someStudent);
 		}
 		
-		someStudent = theHT.removeFromTable(521432);
-		
+		// do we need to check that student numbers are unique?
+		someStudent = theHT.removeFromTable(111120);
 		if (someStudent != null) {
 			System.out.println(someStudent.firstName);
 		}
@@ -48,7 +61,15 @@ public class TestHashTable {
 			System.out.println(someStudent);
 		}
 		
-		theHT.displayTable();
+		someStudent = theHT.removeFromTable(111120);
+		if (someStudent != null) {
+			System.out.println(someStudent.firstName);
+		}
+		else {
+			System.out.println(someStudent);
+		}
+		
+//		theHT.displayTable();
 		
 
 	}
