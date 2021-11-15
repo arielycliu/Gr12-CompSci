19d620167e5d06d1c0156578326cc64c37f103aa
arielycliu <ariel.yc.liu@gmail.com>
2021-11-15 08:54:02 -0500

    FTE and PTE

---
+++ HashTable/src/EmployeeInfo.java
@@ -0,0 +1,31 @@
+
+public class EmployeeInfo {
+	// Attributes
+	
+	public int empNumber;
+	public String firstName;
+	public String lastName;
+	public int gender;
+	public int workLoc;
+	public double deductRate;
+	
+	// Blank Constructors
+	public EmployeeInfo() {
+		this.empNumber = 0;
+		this.firstName = "A";
+		this.lastName = "L";
+		this.gender = 0;
+		this.workLoc = 0;
+		this.deductRate = 0;
+	}
+	
+	// Constructors
+	public EmployeeInfo(int empNumber, String firstName, String lastName, int gender, int workLoc, double deductRate) {
+		this.empNumber = empNumber;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.gender = gender;
+		this.workLoc = workLoc;
+		this.deductRate = deductRate;
+	}
+}
