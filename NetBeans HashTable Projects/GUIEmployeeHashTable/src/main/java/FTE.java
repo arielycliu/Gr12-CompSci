d382e5c7b70a9047ac5792902c48afee010884ab
arielycliu <ariel.yc.liu@gmail.com>
2021-11-16 10:13:06 -0500

    reorganize files and add gui files

---
+++ NetBeans HashTable Projects/GUIEmployeeHashTable/src/main/java/FTE.java
@@ -0,0 +1,20 @@
+
+public class FTE extends EmployeeInfo {
+	
+	// FULL TIME
+	
+	public double salary;
+	
+	public FTE(int eN, String fN, String lN, int g, int wL, double dR, double s) {
+		super(eN, fN, lN, g, wL, dR); // call constructor for employeeInfo
+		salary = s;
+	}
+	
+	public double calcGrossAnnualIncome() {
+		return salary;
+	}
+	
+	public double calcNetAnnualIncome() {
+		return salary * (1.0 - deductRate);
+	}
+}
