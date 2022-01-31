8b8bab9676634f59170223378aa2b2fb1ebb62c6
Ariel <ariel.yc.liu@gmail.com>
2022-01-31 10:13:30 -0500

    s

--- Summative/src/MyPerson.java
+++ Summative/src/MyPerson.java
@@ -17,11 +17,11 @@ public class MyPerson {
 	// Methods
 	public double calcValue() {
 		if (this instanceof MyStudent) { // if the person is an instance of student
-			MyStudent theSquare = (MyStudent) this; // save as student, type cast
-			return theSquare.calcValue(); // run student calcValue function
+			MyStudent theStudent = (MyStudent) this; // save as student, type cast
+			return theStudent.calcValue(); // run student calcValue function
 		} else if (this instanceof MyTeacher) { // if the person is an instance of teacher
-			MyTeacher theTriangle = (MyTeacher) this; // save as teacher, change type
-			return theTriangle.calcValue(); // run teacher calcValue function
+			MyTeacher theTeacher = (MyTeacher) this; // save as teacher, change type
+			return theTeacher.calcValue(); // run teacher calcValue function
 		}
 		return 0.0;
 	}
